38e900c56c04980d72edaa0c3934589191250bc4
Ceres <87485733+ceres0@users.noreply.github.com>
2023-07-18 01:59:00 +0000

    my_mount-9pfs

--- MySolution/baby-steps/03-mount-9pfs/launch.sh
+++ MySolution/baby-steps/03-mount-9pfs/launch.sh
@@ -16,7 +16,7 @@ sudo ifconfig "$BRIDGE_IFACE" "$BRIDGE_IP"
 
 echo "Starting KVM image connected to bridge interface $BRIDGE_IFACE ..."
 sudo qemu-system-x86_64  -fsdev local,id=myid,path=$(pwd)/fs0,security_model=none \
-                         -device virtio-9p-pci,fsdev=myid,mount_tag=rootfs,disable-modern=on,disable-legacy=off \
+                         -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
                          -netdev bridge,id=en0,br=virbr0 \
                          -device virtio-net-pci,netdev=en0 \
                          -kernel "$KVM_IMAGE" \
