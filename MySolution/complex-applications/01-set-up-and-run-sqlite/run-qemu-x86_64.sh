14d999470ca033e9287fbf91da7feee23d7b45c2
Ceres <87485733+ceres0@users.noreply.github.com>
2023-07-19 06:30:04 +0000

    my_set-up-and-run-sqlite

---
+++ MySolution/complex-applications/01-set-up-and-run-sqlite/run-qemu-x86_64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if test $# -ne 1; then
+    echo "Usage: $0 <path_to_kvm_image>" 1>&2
+    exit 1
+fi
+
+KVM_IMAGE="$1"
+
+/usr/bin/qemu-system-x86_64 \
+    -fsdev local,id=myid,path="$(pwd)/fs0",security_model=none \
+    -device virtio-9p-pci,fsdev=myid,mount_tag=fs0,disable-modern=on,disable-legacy=off \
+    -kernel "$KVM_IMAGE" -nographic
